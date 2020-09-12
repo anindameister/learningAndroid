e6dd88693c3e49728327bf7e4b5e14bcc89ae1c4
anindamaulik <aninda.maulik@etu.univ-st-etienne.fr>
2020-09-12 21:21:48 +0200

    realised that I have to go back to oops

--- MyApplication/app/src/main/java/com/anindamaulik/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/anindamaulik/myapplication/MainActivity.java
@@ -12,13 +12,26 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b=(Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Aninda Clicked on the button", Toast.LENGTH_LONG).show();
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
